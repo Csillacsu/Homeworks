d362af7c9906bcb1d469664f1779dd1e8ae1dddd
Szitár Szilvia <szitar.szilvia@progmasters.hu>
2023-02-20 12:42:40 +0100

    házi feladatok

---
+++ Day1/homework/form.py
@@ -0,0 +1,6 @@
+'''
+Feladat: Töltsük ki a form-ot python kód segítségével!
+Szorgalmi: Próbáljuk meg beküldeni az adatokat!
+
+https://testpages.herokuapp.com/styled/validation/input-validation.html
+'''
---
+++ Day1/homework/triangle.py
@@ -0,0 +1,6 @@
+'''
+Feladat: Ellenőrizzük le, megfelelően történik-e a háromszög fajták meghatározása!
+Használjunk elágazást és írjunk ki a képernyőre egy az eredménynek megfelelő szöveget!
+
+https://testpages.herokuapp.com/styled/apps/triangle/triangle001.html
+'''
