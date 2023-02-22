38704ab56cf032fd891e3e0740aaa309cac31464
horandi <hor.andi@gmail.com>
2023-02-22 10:16:53 +0100

    first exercise changes + hootel exercises

--- Day3/practice_exercises/demoqa.py
+++ Day3/practice_exercises/demoqa.py
@@ -1,3 +1,61 @@
 '''
 https://demoqa.com/register
 '''
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+URL = 'https://demoqa.com/register'
+service = Service(executable_path=r'/home/user/Asztal/chromedriver_linux64/chromedriver')
+options = Options()
+options.add_experimental_option("detach", True)
+browser = webdriver.Chrome(service=service, options=options)
+
+browser.get(URL)
+browser.maximize_window()
+
+# first_name = browser.find_element(By.ID, 'firstname')
+# first_name.send_keys("Kovács")
+#
+# last_name = browser.find_element(By.ID, 'lastname')
+# last_name.send_keys("Béla")
+#
+# user_name = browser.find_element(By.ID, 'userName')
+# user_name.send_keys("KGB")
+#
+# password = browser.find_element(By.ID, 'password')
+# password.send_keys("KGB3la")
+#
+# reg_btn = browser.find_element(By.ID, 'register')
+# reg_btn.click()
+#
+# error_msg = browser.find_element(By.ID, 'name')
+# print(error_msg.text)
+#
+# if "Please verify reCaptcha to register!" == error_msg.text:
+#     print("OK")
+# else:
+#     print("Fail")
+
+
+first_name = browser.find_element(By.ID, 'firstname')
+class_before_click = first_name.get_attribute('class')
+print(class_before_click)
+first_name.send_keys(Keys.PAGE_DOWN)
+
+reg_btn = browser.find_element(By.ID, 'register')
+reg_btn.click()
+
+class_after_click = first_name.get_attribute('class')
+print(class_after_click)
+
+# if class_before_click == class_after_click:
+#     print("Hiba")
+# else:
+#     print("Jó működés")
+
+if "is-invalid" in class_after_click:
+    print("Hibaüzenet megjelent")
---
+++ Day3/practice_exercises/hootel.py
@@ -0,0 +1,12 @@
+'''
+http://hotel-v2.progmasters.hu/
+
+1) Navigáljunk el a hotelek listájához.
+2) Ellenőrozzük le a checkboxok működését: mindet kattintsuk be, majd a "Szoba szolgáltatás szűrések törlése" gombra kattintva távolítsuk el a kiválaszott szolgáltatásokat.
+3) Navigáljunk el egy tetszőleges hotel oldalára.
+4) Írjuk ki a terminálra a hosszú leírásást.
+5) Ellenőrizzük le egy elágazással, hogy a hosszú leírás 500 karakternél hosszabb-e (kapjunk róla visszajelzést a terminálon).
+6) Jelentkezzünk be az oldalra.
+7) Vizsgáljuk meg, hogy a felhasználónknak van-e korábbi, vagy jelenleg aktuális foglalása.
+
+'''
