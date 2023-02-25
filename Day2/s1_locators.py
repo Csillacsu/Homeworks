47320e00cc5a467b665a08da2b1746d505688c6c
Lengyel Csilla <lengyel.csilla.90@gmail.com>
2023-02-26 00:18:22 +0100

    ismétlés 2

--- Day2/s1_locators.py
+++ Day2/s1_locators.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 URL = 'http://hotel-v2.progmasters.hu/'
-service = Service(executable_path=r'/home/user/Asztal/chromedriver_linux64/chromedriver')
+service = Service(executable_path=r'C:/Users/Csillacsu/OneDrive/Dokumentumok/Progmasters Automata tesztelő 2023/chromedriver_win32/chromedriver.exe')
 options = Options()
 options.add_experimental_option("detach", True)
 browser = webdriver.Chrome(service=service, options=options)
