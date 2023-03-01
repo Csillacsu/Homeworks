dddc1c04d8bd0efaeb8cc0725bc2c3e45f6550b6
horandi <hor.andi@gmail.com>
2023-03-01 10:13:27 +0100

    Hootel tests added

--- Day5/test/test_hootel.py
+++ Day5/test/test_hootel.py
@@ -14,13 +14,19 @@ class TestHotel(object):
         URL = "http://hotel-v2.progmasters.hu/"
         self.browser.get(URL)
         self.browser.maximize_window()
+
     def teardown_method(self):
         self.browser.quit()
+
+    def test_open(self):
+        page_name = self.browser.find_element(By.ID, 'nav-index')
+        assert page_name.is_displayed()
+        assert page_name.text == "HOOTEL"
 
     def test_login(self):
         signin_btn = self.browser.find_element(By.CSS_SELECTOR, 'a[class="nav-link"]')
         signin_btn.click()
-        email_input = self.browser.find_element(By.ID, 'email')
+        email_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.ID, 'email')))
         password_input = self.browser.find_element(By.ID, 'password')
         login_btn = self.browser.find_element(By.CSS_SELECTOR, 'button[name="submit"]')
         email_input.send_keys('andi.teszt2021@gmail.com')
@@ -28,3 +34,8 @@ class TestHotel(object):
         login_btn.click()
         profile_id = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.ID, 'profile')))
         assert profile_id.text == "Profilom (Andrea)"
+
+    def test_hotellist(self):
+        self.test_login()
+        hotellist_btn = self.browser.find_element(By.XPATH, '//button[@class="btn btn-outline-primary btn-block"]')
+        hotellist_btn.click()
