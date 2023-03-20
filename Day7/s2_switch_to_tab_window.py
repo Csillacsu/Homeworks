3d72f988d32217e0506ae615ebb7bc3270541087
horandi <hor.andi@gmail.com>
2023-03-20 10:23:08 +0100

    switch to

---
+++ Day7/s2_switch_to_tab_window.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+service = Service(executable_path=ChromeDriverManager().install())
+options = Options()
+options.add_experimental_option("detach", True)
+options.add_argument('window-position=-1000,0')
+browser = webdriver.Chrome(service=service, options=options)
+
+URL = "http://selenium.oktwebs.training360.com/kitchensink.html"
+browser.get(URL)
+browser.maximize_window()
