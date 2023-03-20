bdc08fa476792a1e61f6c71dad5dd40f5958f78a
horandi <hor.andi@gmail.com>
2023-03-20 10:56:50 +0100

    switch to alert

--- Day7/s1_switch_to_alert.py
+++ Day7/s1_switch_to_alert.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -15,4 +17,32 @@ browser = webdriver.Chrome(service=service, options=options)
 URL = "http://selenium.oktwebs.training360.com/kitchensink.html"
 browser.get(URL)
 browser.maximize_window()
+
+# alert_btn = browser.find_element(By.ID, 'alertbtn')
+# alert_btn.click()
+#
+# alert_popup = browser.switch_to.alert
+#
+# assert alert_popup.text == "Hello , share this practice page and share your knowledge"
+#
+# alert_popup.accept()
+#
+# alert_btn.click()
+# if EC.alert_is_present():
+#     alert_popup = browser.switch_to.alert
+#     assert alert_popup.text == "Hello , share this practice page and share your knowledge"
+#     alert_popup.accept()
+#     print("Alert accepted")
+# else:
+#     print("No alert present")
+
+# confirm_btn = browser.find_element(By.ID, 'confirmbtn')
+# confirm_btn.click()
+#
+# confirm_popup = browser.switch_to.alert
+# time.sleep(2)
+# # confirm_popup.accept()
+# confirm_popup.dismiss()
+
+
 
