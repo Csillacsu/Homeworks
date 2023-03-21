209f5cd9b6193a352a3b8c5833873b63fb4ad5b7
horandi <hor.andi@gmail.com>
2023-03-21 11:08:47 +0100

    csv reading

--- Day8/s3_csv_reader.py
+++ Day8/s3_csv_reader.py
@@ -1,49 +1,74 @@
+import csv
 
+# with open('movies.csv', 'r') as file:
+#     movies_table = csv.reader(file, delimiter=',')
+#     print(type(movies_table))
+#     print(movies_table)
+#     for row in movies_table:
+#         # print(row)
+#         # print(type(row))
+#         print(row[0])
+#         print(type(row[0]))
 
+# titles = []
+# with open('movies.csv', 'r') as file:
+#     movies_table = csv.reader(file)
+#     next(movies_table)
+#     # next(movies_table)
+#     for row in movies_table:
+#         print(row)
+#         titles.append(row[0])
+#
+# print(titles)
 
-
-
-
-
-
-
-
+# directors = []
+# with open('movies2.csv', 'r') as movies:
+#     movies_table = csv.reader(movies, delimiter=';')
+#     next(movies_table)
+#     for row in movies_table:
+#         print(row)
+#     movies.seek(0)
+#     next(movies_table)
+#     for row in movies_table:
+#         directors.append(row[1])
+#
+# print(directors)
 
 
 ########### Hotel
-# import time
-# import csv
-#
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import Select
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.support.wait import WebDriverWait
-#
-#
-# service = Service(executable_path=ChromeDriverManager().install())
-# options = Options()
-# options.add_experimental_option("detach", True)
-#
-# browser = webdriver.Chrome(service=service, options=options)
-#
-# URL = "http://hotel-v2.progmasters.hu/"
-# browser.get(URL)
-# browser.maximize_window()
-#
-# login_btn = browser.find_element(By.CSS_SELECTOR, 'a[class="nav-link"]')
-# login_btn.click()
-# email_input = browser.find_element(By.ID, 'email')
-# password_input = browser.find_element(By.ID, 'password')
-# submit_btn = browser.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
-#
-# email_input.send_keys('ditoy18234@necktai.com')
-# password_input.send_keys('tesztelek2021')
-# submit_btn.click()
-#
-# time.sleep(2)
-# profile = browser.find_element(By.CSS_SELECTOR, 'a[id="profile"]')
-# assert profile.text == "Profilom (Andrea)"
+import time
+import csv
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+service = Service(executable_path=ChromeDriverManager().install())
+options = Options()
+options.add_experimental_option("detach", True)
+
+browser = webdriver.Chrome(service=service, options=options)
+
+URL = "http://hotel-v2.progmasters.hu/"
+browser.get(URL)
+browser.maximize_window()
+
+login_btn = browser.find_element(By.CSS_SELECTOR, 'a[class="nav-link"]')
+login_btn.click()
+email_input = browser.find_element(By.ID, 'email')
+password_input = browser.find_element(By.ID, 'password')
+submit_btn = browser.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
+
+email_input.send_keys('ditoy18234@necktai.com')
+password_input.send_keys('tesztelek2021')
+submit_btn.click()
+
+time.sleep(2)
+profile = browser.find_element(By.CSS_SELECTOR, 'a[id="profile"]')
+assert profile.text == "Profilom (Andrea)"
