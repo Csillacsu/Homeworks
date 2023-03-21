4d812accd920a5f721529a64b31b533c31016374
horandi <hor.andi@gmail.com>
2023-03-21 12:10:47 +0100

    csv reading with selenium

--- Day8/s3_csv_reader.py
+++ Day8/s3_csv_reader.py
@@ -52,7 +52,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 service = Service(executable_path=ChromeDriverManager().install())
 options = Options()
 options.add_experimental_option("detach", True)
-
+options.add_argument('window-position=-1000,0')
 browser = webdriver.Chrome(service=service, options=options)
 
 URL = "http://hotel-v2.progmasters.hu/"
@@ -72,3 +72,27 @@ submit_btn.click()
 time.sleep(2)
 profile = browser.find_element(By.CSS_SELECTOR, 'a[id="profile"]')
 assert profile.text == "Profilom (Andrea)"
+
+with open('room.csv', 'r') as rooms:
+    room_reader = csv.reader(rooms, delimiter=',')
+    next(room_reader)
+    for room in room_reader:
+        new_room_btn = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//button[@class="btn btn-primary btn-lg btn-block"]')))
+        new_room_btn.click()
+        room_name_input = browser.find_element(By.ID, 'roomName')
+        room_type = Select(browser.find_element(By.ID, 'roomType'))
+        bed_nr = browser.find_element(By.ID, 'numberOfBeds')
+        room_area = browser.find_element(By.ID, 'roomArea')
+        price = browser.find_element(By.ID, 'pricePerNight')
+        description = browser.find_element(By.ID, 'description')
+        save_btn = browser.find_element(By.XPATH, '//button[@class="btn btn-primary my-buttons"]')
+
+        room_name_input.send_keys(room[0])
+        room_type.select_by_value(room[1])
+        bed_nr.send_keys(room[2])
+        room_area.send_keys(room[3])
+        price.send_keys(room[4])
+        description.send_keys(room[5])
+
+        save_btn.click()
+
