ca6ed361972f7fe1f8acdc486af5b5b635318c4a
horandi <hor.andi@gmail.com>
2023-03-21 10:24:11 +0100

    while loop

--- Day8/s2_while_loop.py
+++ Day8/s2_while_loop.py
@@ -16,3 +16,22 @@ browser = webdriver.Chrome(service=service, options=options)
 URL = "http://hotel-v2.progmasters.hu/"
 browser.get(URL)
 browser.maximize_window()
+
+hotel_list_btn = browser.find_element(By.XPATH, '//button[@class="btn btn-outline-primary btn-block"]')
+hotel_list_btn.click()
+time.sleep(2)
+# nav_links = WebDriverWait(browser, 5).until(EC.presence_of_all_elements_located((By.XPATH, '//a[@class="page-link"]')))
+nav_links = browser.find_elements(By.XPATH, '//li[@class="page-item"]')
+#
+# for link in nav_links:
+#     link.click()
+#     print(browser.current_url)
+
+next_button = nav_links[-1]
+# print(next_button.text)
+
+while next_button.get_attribute("class") != "page-item disabled":
+    next_button.click()
+    print(browser.current_url)
+    time.sleep(1)
+
