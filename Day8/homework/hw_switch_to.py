e13f9d25cfdb76947df6b24aaadcba82fd9bdf78
horandi <hor.andi@gmail.com>
2023-03-21 10:24:25 +0100

    homework

---
+++ Day8/homework/hw_switch_to.py
@@ -0,0 +1,8 @@
+'''
+Feladat:
+https://the-internet.herokuapp.com/iframe
+Az oldalon található szövegszerkesztő kitöltésének automatizálása.
+A beviteli mezőt a https://www.lipsum.com/ oldalon generált szöveggel töltsük ki (nyissuk meg új tabon az oldalt és másoljuk át a generált textet)
+
+(segítség: a szövegszerkesztő beviteli mezője az adott frame body tag-jével azonosítható be)
+'''
